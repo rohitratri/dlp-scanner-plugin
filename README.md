048280d517013977ace4044a4d4dec5c5bccc204
Rohit Atri <ratri@cisco.com>
2017-12-07 11:35:18 -0800

    Initial commit - needs cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# dlp-scanner-plugin
-Firefox plugin to detect PII in webpages
---
+++ background.js
@@ -0,0 +1,7 @@
+
+function alertme(){
+    console.log('Hello!');
+}
+
+// Listener to check for submit events
+document.addEventListener('DOMContentLoaded', alertme);
